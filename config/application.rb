cc0260a7f8f25f73eb4eac833686eb8e44244b8c
Taufan Arif <hadikusumaa15@gmail.com>
2020-01-20 16:16:10 +0700

    start creating spec

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,16 @@ module MarketPlaceApi
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.view_specs false
+      g.helper_specs false
+      g.stylesheets = false
+      g.javascripts = false
+      g.helper = false
+    end
+  
+    config.autoload_paths += %W(\#{config.root}/lib)
   end
 end
