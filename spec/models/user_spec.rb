7ca7d6eeb2b00fbedaf8e47f42ecb32a8f4c5bc1
Taufan Arif <hadikusumaa15@gmail.com>
2020-03-16 03:46:29 +0700

    add spec validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,8 +9,17 @@ RSpec.describe User, type: :model do
     expect(subject).to respond_to(:email) 
   end
 
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_confirmation_of(:password) }
+  it { should allow_value('example@domain.com').for(:email) }
+
   it { expect(subject).to respond_to(:password) }
   it { expect(subject).to respond_to(:password_confirmation) }
-
   it { expect(subject).to be_valid }
+
+  describe "when email is not present" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
 end
