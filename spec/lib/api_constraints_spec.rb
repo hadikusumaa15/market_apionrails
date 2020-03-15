6947fff88cab5b3cd4f3a97524a423d5fa116399
Taufan Arif <hadikusumaa15@gmail.com>
2020-03-15 21:01:31 +0700

    setting_api
    add api constraints service

--- spec/lib/api_constraints_spec.rb
+++ spec/lib/api_constraints_spec.rb
@@ -1,20 +1,22 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe ApiConstraints do
+# describe ApiConstraints do
+RSpec.describe ApiConstraints do
   let(:api_constraints_v1) { ApiConstraints.new(version: 1) }
   let(:api_constraints_v2) { ApiConstraints.new(version: 2, default: true) }
 
   describe "matches?" do
-
+    # kalau versionnya tidak default maka harus menggunakan header untuk menerima versi 1
     it "returns true when the version matches the 'Accept' header" do
       request = double(host: 'api.marketplace.dev',
                        headers: {"Accept" => "application/vnd.marketplace.v1"})
-      api_constraints_v1.matches?(request).should be_true
+      api_constraints_v1.matches?(request).should be_truthy
     end
 
+    # kalau versionnya default maka TIDAK harus menggunakan header
     it "returns the default version when 'default' option is specified" do
       request = double(host: 'api.marketplace.dev')
-      api_constraints_v2.matches?(request).should be_true
+      api_constraints_v2.matches?(request).should be_truthy
     end
   end
 end
