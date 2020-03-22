9e47e859af048bfca047c24b3d302749a0255c0d
Taufan Arif <hadikusumaa15@gmail.com>
2020-03-22 17:39:28 +0700

    baru

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
                               constraints: { subdomain: 'api' }, path: '/'  do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
-      resources :users, :only => [:show]
+      resources :users, :only => [:show, :index]
     end
   end
 end
+# curl -H 'Accept: application/vnd.marketplace.v1' \ 
+# http://api.market_place_api.dev/users/1
+# http://api.lvh.me:3000/users/1
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -7,9 +7,12 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     before(:each) do
       @user = FactoryGirl.create :valid_user
       get :show, params: { id: @user.id }, format: :json
+      # cara baca di localhost gini:
+      # http://api.lvh.me:3000/users/1
     end
 
     it "returns the information about a reporter on a hash" do
+      byebug
       user_response = JSON.parse(response.body, symbolize_names: true)
       expect(user_response[:email]).to eql @user.email
     end
